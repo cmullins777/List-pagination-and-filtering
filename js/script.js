7e352909a6924f719ddc4d5b90a4d00c908ca220
Cindy Mullins <mullins.cindy@gmail.com>
2019-03-12 20:59:56 -0700

    call showPage once to display first 10 items

--- js/script.js
+++ js/script.js
@@ -6,72 +6,49 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 /***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
-
-//const studentArray = [];
-
-//console.log(totalItems);
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function``````````````````````````````
+  Declare global variables, including;
+  html tags containing student data,
+  pageSize, a dynamic variable for number of items per page,
+  page, to be called once in showPage() when page loads
 ***/
 let students = document.querySelectorAll('.student-item');
-console.log(students);
 let totalItems = students.length;
-console.log(totalItems);
 const pageSize = 10;
+let a = document.getElementsByTagName("a");
+let page = parseInt(a.textContent);
 
+/***
+  Create showPage function, pass in student list and selected page,
+  dynamically determine which items are shown by page selection and pageSize
+  show selected items, hide unselected
+***/
 const showPage = (list, page) => {
-  for (let i = 0; i <= students.length; i++) {
-    console.log(students);
-    if (i >= ((page * pageSize) - pageSize) && (i <= (page * pageSize))) {
+  for (let i = 0; i < students.length; i++) {
+    if (i >= ((page * pageSize) - pageSize) && (i < (page * pageSize))) {
       students[i].style.display = "block";
     } else {
       students[i].style.display = "none";
     }
   }
 };
-
+showPage(students, [1]);
 /***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
+   Create appendPage function,
+   create page links dynamically for number of totalPages and append to ul, append to page div
+   add event listener to page links to call showPage function
 ***/
 const appendPageLinks = (list) => {
   let totalPages = Math.floor(totalItems/pageSize);
 
   const pageDiv = document.createElement("div");
   pageDiv.className = 'pagination';
-  //pageDiv.innerHTML = "I'm pageDiv";
   document.getElementsByClassName("page")[0].appendChild(pageDiv);
 
   const pageUl = document.createElement("ul");
   pageUl.className = 'pagination';
-  //pageUl.innerHTML = "I'm the Ul";
   pageDiv.appendChild(pageUl);
 
-  for (i = 0; i<=totalPages; i++) {
+  for (let i = 0; i<=totalPages; i++) {
     const li = document.createElement("li");
     const a = document.createElement("a");
     pageDiv.appendChild(li);
@@ -80,22 +57,12 @@ const appendPageLinks = (list) => {
     li.className = 'pagination li';
     a.className = 'pagination li a';
     a.innerHTML = [i+1];
-    page = a.textContent;
-    console.log(page);
+    let page = parseInt(a.textContent);
     a.addEventListener('click', (e) => {
       a.classList.remove('active');
-      showPage(students, page);
+      showPage(students, e.target.textContent);
     });
   };
-
-
-  //  for (i = 0; i<=totalPages.length; i++) {
-  //    a.remove('active');
-  //  };
 };
 
 appendPageLinks();
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
